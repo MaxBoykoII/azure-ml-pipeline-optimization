fc68c525b0ada6bd6d20cdf85f251d652125681d
Max Boyko <maxboykomax@gmail.com>
2021-03-25 00:22:48 -0700

    Add remaining dependency and begin fixing issues in training script

--- train.py
+++ train.py
@@ -16,15 +16,6 @@ ds = TabularDatasetFactory.from_delimited_files(
     path=web_url, separator=",", header=True
 )
 
-x, y = clean_data(ds)
-
-# Split data into train and test sets.
-x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.33, random_state=42
-)
-
-run = Run.get_context()
-
 
 def clean_data(data):
     # Dict for cleaning data
@@ -64,6 +55,16 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+
+
+x, y = clean_data(ds)
+
+# Split data into train and test sets.
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.33, random_state=42
+)
+
+run = Run.get_context()
 
 
 def main():
