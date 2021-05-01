3f519f9266a46f6f809fcc1e86ecbd2534f17e69
Max Boyko <maxboykomax@gmail.com>
2021-05-01 01:02:42 -0700

    Extend training script and notebook code to support saving the best hyperdrive model

--- train.py
+++ train.py
@@ -97,6 +97,9 @@ def main():
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
 
+    os.makedirs("./outputs", exist_ok=True)
+    joblib.dump(value=model, filename="./outputs/hyperdrive.joblib")
+
 
 if __name__ == "__main__":
     main()
